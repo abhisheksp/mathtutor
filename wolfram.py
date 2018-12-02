6674e59f7ac5c25bf4bc98c45d9aef2c7b5f606d
Abhishek S P <abhisheksp1993@gmail.com>
2018-12-02 01:41:28 -0500

    add fullresults component

--- wolfram.py
+++ wolfram.py
@@ -1,3 +1,5 @@
+import json
+
 import requests
 
 
@@ -8,8 +10,23 @@ def find_roots(question):
     return res.text
 
 
-def analyze(zip_step):
-    question, steps = zip_step
+def full_results(latex_exp):
+    params = {'exp': latex_exp}
+    full_form_url = 'https://www.wolframcloud.com/objects/b6228cee-58da-43d0-9c98-077a390614b5'
+    res = requests.get(full_form_url, params=params)
+    input_exp = 'solve {}'.format(res.text)
+    full_results_url = 'http://api.wolframalpha.com/v2/query?appid=QU7434-QE6KPPYTAX&format=plaintext&podstate=Result__Step-by-step%20solution&output=json&input=' + input_exp
+    res = requests.get(full_results_url)
+    data = json.loads(res.text)
+    for pod in data['queryresult']['pods']:
+        if pod['id'] == 'Result':
+            for subpod in pod['subpods']:
+                if subpod['title'] == 'Possible intermediate steps':
+                    solution_string = subpod['plaintext']
+    return solution_string.splitlines()
+
+
+def analyze(question, steps):
     url = 'https://www.wolframcloud.com/objects/b269a111-9f38-42b3-8b11-bc01b2564be4'
     step_num = None
     for i, step in enumerate(steps):
@@ -21,7 +38,8 @@ def analyze(zip_step):
             print('{} != {}'.format(question, step))
             break
     if step_num is None:
-        response = {'correct': True}
+        result = full_results(question)
+        response = {'correct': True, 'reference_solution': result}
     else:
         response = {'correct': False, 'error': step_num}
     return response
@@ -30,6 +48,14 @@ def analyze(zip_step):
 def solve(latex_equations):
     questions = list(map(lambda x: x[0], latex_equations))
     # roots = list(map(find_roots, questions))
-    analysis_responses = list(map(analyze, zip(questions, latex_equations)))
-    response = {'correct': True, 'reference_solution': '', 'analysis': analysis_responses}
-    return response
+    # analysis_responses = list(map(analyze, zip(questions, latex_equations)))
+    responses = []
+    for question, steps in zip(questions, latex_equations):
+        analysis = analyze(question, steps)
+        student_solution = steps
+        response = {'question': question, 'analysis': analysis, 'student_solution': student_solution}
+        responses.append(response)
+    return responses
+
+    # response = {'correct': True, 'reference_solution': '', 'analysis': analysis_responses, 'full_result': }
+    # return response
