cb6e5c3aa0da7772a36b0ac87302e8600e48d83d
Abhishek S P <abhisheksp1993@gmail.com>
2018-12-01 19:31:13 -0500

    add ocr component

--- ocr.py
+++ ocr.py
@@ -1,2 +1,28 @@
-def parse_images(images_url):
-    return 'blah'
+import json
+import requests
+
+with open('mathpix.json') as f:
+    data = json.load(f)
+    app_id = data['app_id']
+    app_key = data['app_key']
+
+
+def parse_images(image_urls):
+    responses = []
+    for image_url in image_urls:
+        request = requests.post(
+            "https://api.mathpix.com/v3/latex",
+            data=json.dumps({'src': image_url}),
+            headers={
+                "app_id": app_id,
+                "app_key": app_key,
+                "Content-type": "application/json"}
+        )
+        response = json.loads(request.text)
+        responses.append(response['latex'])
+    return responses
+
+
+images = ['https://raw.githubusercontent.com/HieuNgo/math_tutor/master/api-examples/images/test.jpg',
+          'https://raw.githubusercontent.com/HieuNgo/math_tutor/master/api-examples/images/test.jpg']
+print(parse_images(images))
